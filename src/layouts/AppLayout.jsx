9431372563e59ba51ad6c9bda521b57c8232ec70
Karan Singh <karansingh14032@gmail.com>
2024-06-30 14:05:16 +0530

    Modified global css

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,11 +4,13 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      <main>
+      <main className="min-h-screen">
         {/* Header */}
         <Outlet />
       </main>
-      <footer>{/* Footer */}</footer>
+      <footer className="text-center p-5 text-white bg-gray-800">
+        <div>Footer for our page</div>
+      </footer>
     </div>
   );
 };
